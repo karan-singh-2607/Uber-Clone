38e972b29dad4869d8a138fcbd5ea2b9b3b6d62c
karan-singh-2607 <karan.2607.github@gmail.com>
2022-01-17 20:55:43 +0530

    On destinationn search 1) google place autocomplete added 2) auto navigation functionality added using useeffect

--- src/Screens/WhereToScreen/index.js
+++ src/Screens/WhereToScreen/index.js
@@ -1,14 +1,51 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { StyleSheet, Text, TextInput, View } from 'react-native'
-import themeColors from '../../Assets/Colors/theme.colors'
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 
+import themeColors from '../../Assets/Colors/theme.colors'
+const GOOGLE_API_KEY = 'AIzaSyDh7W90w22SlO5YlSFtxK7piIbXZAj-Krs'
 const WhereToScreen = () => {
-    const [FromAddress, setFromAddress] = useState('');
-    const [DestinationAddress, setDestinationAddress] = useState('')
+    const [SourceAddress, setSourceAddress] = useState(null);
+    const [DestinationAddress, setDestinationAddress] = useState(null)
+
+    useEffect(() => {
+        console.log('useeffect called');
+        if (SourceAddress && DestinationAddress) {
+            console.log('redirect')
+        }
+    }, [SourceAddress, DestinationAddress])
     return (
         <View style={styles.SearchContainer}>
-            <TextInput placeholder='From' value={FromAddress} onChangeText={setFromAddress} style={styles.InputContainer} />
-            <TextInput placeholder='Where to?' value={DestinationAddress} onChangeText={setDestinationAddress} style={styles.InputContainer} />
+            <GooglePlacesAutocomplete
+                placeholder='Where to?'
+                onPress={(data, details = null) => {
+                    // 'details' is provided when fetchDetails = true
+                    setSourceAddress({ data, details })
+                    console.log(data, details);
+                }}
+                fetchDetails
+                styles={{ textInput: styles.InputContainer }}
+                onFail={(error) => console.error(error)}
+                query={{
+                    key: GOOGLE_API_KEY,
+                    language: 'en',
+                }}
+            />
+            <GooglePlacesAutocomplete
+                placeholder='Where to?'
+                onPress={(data, details = null) => {
+                    // 'details' is provided when fetchDetails = true
+                    setDestinationAddress({ data, details })
+                    console.log(data, details);
+                }}
+                fetchDetails
+                styles={{ textInput: styles.InputContainer }}
+                onFail={(error) => console.error(error)}
+                query={{
+                    key: GOOGLE_API_KEY,
+                    language: 'en',
+                }}
+            />
         </View>
     )
 }
@@ -17,11 +54,13 @@ export default WhereToScreen
 
 const styles = StyleSheet.create({
     SearchContainer: {
-        padding: 15
+        padding: 15,
+        height: '100%',
     },
     InputContainer: {
         backgroundColor: themeColors.Gray,
         padding: 10,
-        marginBottom: 10
+        marginBottom: 10,
+        borderRadius: 0
     }
 })
